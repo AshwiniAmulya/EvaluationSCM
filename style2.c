b3c2caffb4b3892ccd049ffd30f46bde6ad67b51
AshwiniAmulya <ashwini.r1@s.amity.edu>
2025-05-07 17:42:03 +0530

    html

---
+++ style2.c
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Simple Page</title>
+</head>
+<body>
+  <h1>Hello!</h1>
+  <p>This is the first paragraph.</p>
+  <p>This is the second paragraph.</p>
+  <p>This is the third paragraph.</p>
+</body>
+</html>

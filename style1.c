9509f6314e954b54e30e3a4a1754962a67374cd6
AshwiniAmulya <ashwini.r1@s.amity.edu>
2025-05-07 17:39:44 +0530

    html

---
+++ style1.c
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>My Simple Webpage</title>
+</head>
+<body>
+  <h1>Welcome to My Website</h1>
+  <p>This is a simple HTML page.</p>
+  <p>Here is another paragraph with more information about the site.</p>
+  <p>This is a third paragraph to show how multiple sections of text can be added.</p>
+</body>
+</html>

0e54964b5c87f11598e1af6b3baf526d6fbb51d8
AshwiniAmulya <ashwini.r1@s.amity.edu>
2025-05-07 17:37:59 +0530

    html

--- style.c
+++ style.c
@@ -6,5 +6,8 @@
 <body>
   <h1>Welcome to My Website</h1>
   <p>webpage</p>
+  <p>This is a simple HTML page.</p>
+  <p>Here is another paragraph with more information about the site.</p>
+
 </body>
 </html>
